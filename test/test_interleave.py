80fd92ba155bc056043f8f3c3333ade3499f4d6e
Sam Mayer <samayer12@gmail.com>
2020-06-21 21:26:31 -0500

    Fix file path for pytest

--- test/test_interleave.py
+++ test/test_interleave.py
@@ -2,11 +2,16 @@ import unittest
 from src import interleave
 
 
-class MyTestCase(unittest.TestCase):
+class PDFTests(unittest.TestCase):
     def test_opens_PDF(self):
         self.assertEqual('1. First Entry\n\n'
                          '2. Second Entry\n\n'
-                         '3. Third Entry\n\n\f', interleave.convert_pdf_to_txt('../test/PDFs/Simple.pdf'))
+                         '3. Third Entry\n\n\f', interleave.convert_pdf_to_txt('./test/PDFs/Simple.pdf'))
 
 if __name__ == '__main__':
-    unittest.main()
+    loader = unittest.TestLoader()
+    start_dir = '.'
+    suite = loader.discover(start_dir)
+
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
