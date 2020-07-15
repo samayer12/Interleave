6575ba08ff3f889a9cb91341d412a6594ab96270
Sam Mayer <samayer12@gmail.com>
2020-07-15 14:41:52 -0500

    Write source file paths to CSV file

--- src/interleave.py
+++ src/interleave.py
@@ -43,9 +43,9 @@ def zip_sentences(list1, list2):
     return list(itertools.zip_longest(list1, list2))
 
 
-def create_csv(data, path):
+def create_csv(data, path, source_tuple):
     with open(path, 'w', newline='') as csvfile:
-        writer(csvfile, delimiter=',').writerows([('Document1', 'Document2')])
+        writer(csvfile, delimiter=',').writerows([source_tuple])
         writer(csvfile, delimiter=',').writerows(data)
     return 'Files created.'
 
@@ -78,7 +78,7 @@ def main(argv):
         text_second_file = convert_pdf_to_txt(args.input[1])
 
         filtered_text = zip_sentences(get_sentences(text_first_file), get_sentences(text_second_file))
-        print(create_csv(filtered_text, args.output[0]))
+        print(create_csv(filtered_text, args.output[0], (args.input[0], args.input[1])))
 
 
 if __name__ == '__main__':
