b29938bc23b67e4ee9a30830d9447fc4d2a785a7
Sam Mayer <samayer12@gmail.com>
2020-07-21 11:55:19 -0500

    Add more journey tests

--- test/test_journey.py
+++ test/test_journey.py
@@ -11,10 +11,28 @@ class PDFJourneyTests(unittest.TestCase):
 
         cls.complex_PDF_2 = textract.process('PDFs/Complex_2.pdf', method='pdfminer').decode()
 
-    def test_counts_correct_amount_of_paragraphs_for_complex_documents(self):
+        cls.complex_PDF_3 = textract.process('PDFs/Complex_3.pdf', method='pdfminer').decode()
+
+        cls.complex_PDF_4 = textract.process('PDFs/Complex_4.pdf', method='pdfminer').decode()
+
+        cls.complex_PDF_5 = textract.process('PDFs/Complex_5.pdf', method='pdfminer').decode()
+
+        cls.complex_PDF_6 = textract.process('PDFs/Complex_6.pdf', method='pdfminer').decode()
+
+    def test_counts_correct_amount_of_paragraphs_for_complex_12(self):
         result = interleave.zip_sentences(interleave.build_paragraphs(interleave.sanitize_text(self.complex_PDF_1)),
                                           interleave.build_paragraphs(interleave.sanitize_text(self.complex_PDF_2)))
         self.assertEqual(166, len(result))
+
+    def test_counts_correct_amount_of_paragraphs_for_complex_34(self):
+        result = interleave.zip_sentences(interleave.build_paragraphs(interleave.sanitize_text(self.complex_PDF_3)),
+                                          interleave.build_paragraphs(interleave.sanitize_text(self.complex_PDF_4)))
+        self.assertEqual(64, len(result))
+
+    def test_counts_correct_amount_of_paragraphs_for_complex_56(self):
+        result = interleave.zip_sentences(interleave.build_paragraphs(interleave.sanitize_text(self.complex_PDF_5)),
+                                          interleave.build_paragraphs(interleave.sanitize_text(self.complex_PDF_6)))
+        self.assertEqual(69, len(result))
 
 
 if __name__ == '__main__':
